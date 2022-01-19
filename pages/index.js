24c249b969d1e1a5082453d39aacf8bc17af1500
boostup <boostup@gmail.com>
2022-01-19 16:39:36 +0100

    centering index page contents

--- pages/index.js
+++ pages/index.js
@@ -53,8 +53,7 @@ export default function Home({ propertyForRent, propertyForSale }) {
           "https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
         }
       />
-      <Flex flexWrap={"wrap"}>
-        {/* Fetch the real estate properties and map over them...  */}
+      <Flex flexWrap={"wrap"} justifyContent="center" alignItems="center">
         {propertyForRent.map((property) => (
           <Property property={property} key={property.id} />
         ))}
@@ -71,8 +70,7 @@ export default function Home({ propertyForRent, propertyForSale }) {
           "https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
         }
       />
-      <Flex flexWrap={"wrap"}>
-        {/* Fetch the real estate properties and map over them...  */}
+      <Flex flexWrap={"wrap"} justifyContent="center" alignItems="center">
         {propertyForSale.map((property) => (
           <Property property={property} key={property.id} />
         ))}
