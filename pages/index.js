1fc618dba26db46566e3d1f1c0dc5321d822aa7b
boostup <boostup@gmail.com>
2022-01-20 10:47:58 +0100

    remove useless code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Flex, Box, Text, Buton, Button } from "@chakra-ui/react";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import Property from "../components/Property";
